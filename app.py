482627595c82c76bcd7e8e3ed32a84767558eb25
Laxya Kumar <laxya20supercell@gmail.com>
2023-06-13 10:55:36 -0700

    new

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from sklearn.metrics import mean_squared_error, r2_score
 from streamlit_option_menu import option_menu
 from streamlit_chat import message
 import openai
-
+import sqlite3
+from streamlit_tags import st_tags
+from datetime import datetime
 
 st.markdown(
     """
@@ -19,7 +21,7 @@ st.markdown(
     }
 
     .title {
-        align-self: flex-start;
+        align-self: flex-start;  
      </style>
     """,
     unsafe_allow_html=True
@@ -38,11 +40,142 @@ selected_page = option_menu(
 )
 
 
-# Render the selected page
+
+
 if selected_page == "BizMatch":
+    conn = sqlite3.connect('investors.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS investors
+                (name TEXT, description TEXT, funding REAL, industry TEXT, contact TEXT)''')
+    conn.commit()
+
+    # Page 1: Investor Profile
+    def investor_profile():
+        st.title("Investor Profile")
+        name = st.text_input("Name")
+        description = st.text_area("Description")
+        funding = st.number_input("Funding Amount", min_value=0.0)
+        interests = st.text_input("Interested Industries (comma-separated)")
+        contact = st.text_input("Contact Information")
+        if st.button("Submit"):
+            interests_list = [interest.strip().lower() for interest in interests.split(",")]
+            for interest in interests_list:
+                c.execute("INSERT INTO investors VALUES (?, ?, ?, ?, ?)",
+                        (name, description, funding, interest, contact))
+            conn.commit()
+            st.success("Profile submitted successfully!")
+
+    # Page 2: Startup Founder
+    def startup_founder():
+        st.title("Startup Founder")
+        industries = set()
+        # Fetch all unique industries from the database
+        for row in c.execute("SELECT DISTINCT industry FROM investors"):
+            industries.add(row[0])
+        selected_industry = st.selectbox("Select an Industry", list(industries))
+        if st.button("Find Investors"):
+            # Fetch investors with similar interests from the database (case-insensitive)
+            c.execute("SELECT * FROM investors WHERE LOWER(industry) = LOWER(?)", (selected_industry,))
+            results = c.fetchall()
+            st.subheader("Matching Investors:")
+            for result in results:
+                st.write("Name:", result[0])
+                st.write("Description:", result[1])
+                st.write("Funding:", result[2])
+                st.write("Contact:", result[4])
+                st.write("---")
+
+    # Streamlit App
+    def main():
+        st.header("Investor-Founder Matching App")
+        page = st.sidebar.selectbox("Select a page", ("Investor Profile", "Startup Founder"))
+
+        if page == "Investor Profile":
+            investor_profile()
+        elif page == "Startup Founder":
+            startup_founder()
+
+    if __name__ == '__main__':
+        main()
+ 
+
+  
+    
     # Render the Startup Matchmaker page
-    st.title("Startup Matchmaker")
-    # Add your code for the Startup Matchmaker page here
+#     st.header("Get Matched With Your Ideal Startup Team")
+
+#     # Collect Personal Information
+#     name = st.text_input("Name")
+#     age = st.number_input("Age", min_value=0, max_value=100, value=0, step=1)
+#     location = st.text_input("Location")
+#     contact_information = st.text_area("Contact Information")
+
+#     # Collect Skills and Expertise
+
+#     skills = st_tags(
+#     label='# Enter Skills:',
+#     text='Press enter to add more',
+#     suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+#     maxtags=10,
+#     key='1'
+# )
+
+# # Convert the list of skills into a comma-separated string
+#     expertise = st_tags(
+#     label='# Enter areas of expertise:',
+#     text='Press enter to add more',
+#     suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+#     maxtags=10,
+#     key='2'
+# )
+
+#     qualifications = st_tags(
+#     label='# Enter Qualifications:',
+#     text='Press enter to add more',
+#     suggestions=['five', 'six', 'seven', 'eight', 'nine', 'three', 'eleven', 'ten', 'four'],
+#     maxtags=10,
+#     key='3'
+# )
+
+#     # Collect Interests and Goals
+ 
+#     # Collect Industry Experience
+#     previous_work_experience = st.radio(
+#     "Have you worked before?",
+#     ('Yes', 'No'))
+
+#     if previous_work_experience == 'Yes':
+#         workYears = st.number_input('Years Worked', min_value=0, max_value=100, value=0, step=1)
+
+#     # Collect Business Idea or Concept
+#     business_idea = st.text_area("Business Idea or Concept")
+
+#     # Collect Preferences and Criteria
+#     geographic_location = st.text_input("Geographic Location")
+#     commitment_level = st.selectbox("Desired Level of Commitment", ["Full-time", "Part-time"])
+#     equity_split = st.number_input("Desired Equity Split")
+
+#     # Store the data
+#     user_data = {
+#         "name": name,
+#         "age": age,
+#         "location": location,
+#         "contact_information": contact_information,
+#         "skills": skills,
+#         "expertise": expertise,
+#         "qualifications": qualifications,
+#         "previous_work_experience": previous_work_experience,
+#         "workYears": workYears,
+#         "business_idea": business_idea,
+#         "geographic_location": geographic_location,
+#         "commitment_level": commitment_level,
+#         "equity_split": equity_split,
+#     }
+
+    
+
+    # You can then store this user_data dictionary in a database or use it for further processing
+
 
 elif selected_page == "BizBot":
     openai.api_key = "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"
