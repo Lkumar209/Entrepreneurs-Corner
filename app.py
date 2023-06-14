0933e134f63c756db5e40f7dcc446aa826febfc0
Pranav <pranav.belligundu@gmail.com>
2023-06-13 17:24:19 -0700

    updates

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ import sqlite3
 from streamlit_tags import st_tags
 from datetime import datetime
 
+from ta.volatility import BollingerBands
+from ta.trend import MACD, EMAIndicator, SMAIndicator
+from ta.momentum import RSIIndicator
+import datetime
+from datetime import date
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import r2_score, mean_absolute_error
+
 st.markdown(
     """
     <style>
@@ -21,7 +35,7 @@ st.markdown(
     }
 
     .title {
-        align-self: flex-start;  
+        align-self: flex-start;
      </style>
     """,
     unsafe_allow_html=True
@@ -40,8 +54,6 @@ selected_page = option_menu(
 )
 
 
-
-
 if selected_page == "BizMatch":
     conn = sqlite3.connect('investors.db')
     c = conn.cursor()
@@ -58,10 +70,11 @@ if selected_page == "BizMatch":
         interests = st.text_input("Interested Industries (comma-separated)")
         contact = st.text_input("Contact Information")
         if st.button("Submit"):
-            interests_list = [interest.strip().lower() for interest in interests.split(",")]
+            interests_list = [interest.strip().lower()
+                              for interest in interests.split(",")]
             for interest in interests_list:
                 c.execute("INSERT INTO investors VALUES (?, ?, ?, ?, ?)",
-                        (name, description, funding, interest, contact))
+                          (name, description, funding, interest, contact))
             conn.commit()
             st.success("Profile submitted successfully!")
 
@@ -72,10 +85,12 @@ if selected_page == "BizMatch":
         # Fetch all unique industries from the database
         for row in c.execute("SELECT DISTINCT industry FROM investors"):
             industries.add(row[0])
-        selected_industry = st.selectbox("Select an Industry", list(industries))
+        selected_industry = st.selectbox(
+            "Select an Industry", list(industries))
         if st.button("Find Investors"):
             # Fetch investors with similar interests from the database (case-insensitive)
-            c.execute("SELECT * FROM investors WHERE LOWER(industry) = LOWER(?)", (selected_industry,))
+            c.execute(
+                "SELECT * FROM investors WHERE LOWER(industry) = LOWER(?)", (selected_industry,))
             results = c.fetchall()
             st.subheader("Matching Investors:")
             for result in results:
@@ -97,9 +112,8 @@ if selected_page == "BizMatch":
             default_index=0,
             orientation="horizontal",
             key="nav_bar"
-)
+        )
         st.markdown("<br>", unsafe_allow_html=True)  # Add a line break
-
 
         if page == "Investor Profile":
             investor_profile()
@@ -108,10 +122,7 @@ if selected_page == "BizMatch":
 
     if __name__ == '__main__':
         main()
- 
 
-  
-    
     # Render the Startup Matchmaker page
 #     st.header("Get Matched With Your Ideal Startup Team")
 
@@ -149,7 +160,7 @@ if selected_page == "BizMatch":
 # )
 
 #     # Collect Interests and Goals
- 
+
 #     # Collect Industry Experience
 #     previous_work_experience = st.radio(
 #     "Have you worked before?",
@@ -183,13 +194,11 @@ if selected_page == "BizMatch":
 #         "equity_split": equity_split,
 #     }
 
-    
-
     # You can then store this user_data dictionary in a database or use it for further processing
 
 
 elif selected_page == "BizBot":
-    openai.api_key = "KEY"
+    openai.api_key = "insert the key here !!!"
 
     def generate_response(prompt):
         response = openai.ChatCompletion.create(
@@ -199,31 +208,32 @@ elif selected_page == "BizBot":
         generated_text = response.choices[0].message.content
 
         return generated_text
-    
+
     st.header("BizBot - Your AI Business Assistant")
 
     if 'generated' not in st.session_state:
         st.session_state['generated'] = []
 
-    if 'past' not in st.session_state: 
+    if 'past' not in st.session_state:
         st.session_state['past'] = []
-    
+
     def get_text():
-        input_text = st.text_input("You: ", "How can I get in touch with startup incubators?", key="input")
+        input_text = st.text_input(
+            "You: ", "How can I get in touch with startup incubators?", key="input")
         return input_text
-    
+
     user_input = get_text()
 
     if user_input:
         output = generate_response(user_input)
         st.session_state.past.append(user_input)
         st.session_state.generated.append(output)
-    
+
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['past'][i],
+                    is_user=True, key=str(i) + '_user')
             message(st.session_state['generated'][i], key=str(i))
-            
 
 
 elif selected_page == "Idea Oasis":
@@ -239,47 +249,51 @@ elif selected_page == "EnvisionX":
         # Retrieve historical stock data
         stock = yf.Ticker(symbol)
         data = stock.history(period="1y")
-        
+
         # Prepare the data for prediction
         data = data.reset_index()
         data['Date'] = pd.to_datetime(data['Date'])
         data['Date'] = data['Date'].map(lambda x: x.toordinal())
-        
+
         # Split the data into training and testing sets
         X = data[['Date']].values
         y = data['Close'].values
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-        
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=0)
+
         # Train the linear regression model
         model = LinearRegression()
         model.fit(X_train, y_train)
-        
+
         # Make predictions on the test set
         y_pred = model.predict(X_test)
-        
+
         # Calculate evaluation metrics
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
-        
+
         # Create a DataFrame for predicted values
-        predictions = pd.DataFrame({'Date': data['Date'].values[X_train.shape[0]:], 'Close': y_pred})
-        
+        predictions = pd.DataFrame(
+            {'Date': data['Date'].values[X_train.shape[0]:], 'Close': y_pred})
+
         # Create a combined DataFrame for plotting
         plot_data = pd.concat([data, predictions], ignore_index=True)
-        
+
         # Generate the graph
         plt.figure(figsize=(10, 6))
-        plt.plot(plot_data['Date'], plot_data['Close'], color='blue', label='Actual')
-        plt.plot(plot_data['Date'], plot_data['Close'].shift(-X_train.shape[0]), color='orange', linestyle='--', label='Predicted')
+        plt.plot(plot_data['Date'], plot_data['Close'],
+                 color='blue', label='Actual')
+        plt.plot(plot_data['Date'], plot_data['Close'].shift(-X_train.shape[0]),
+                 color='orange', linestyle='--', label='Predicted')
         plt.xlabel('Date')
         plt.ylabel('Close Price')
         plt.title('Stock Performance and Predictions')
         plt.legend()
         plt.grid(True)
-        
+
         # Display the graph
         st.pyplot(plt)
-        
+
         # Display the results
         st.write("Stock Data:")
         st.write(data)
